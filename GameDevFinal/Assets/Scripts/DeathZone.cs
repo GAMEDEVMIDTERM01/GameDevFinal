77896fd5a6d9abeeb605c19087f94ad95229c9cb
ljobrien <ljoa2021@mymail.pomona.edu>
2024-03-25 15:24:46 +0100

    edits

--- GameDevFinal/Assets/Scripts/DeathZone.cs
+++ GameDevFinal/Assets/Scripts/DeathZone.cs
@@ -6,24 +6,14 @@ using UnityEngine.SceneManagement;
 public class DeathZone : MonoBehaviour
 {
 
-    public float delayBeforeReload = 1f;
-
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            // Perform any death-related actions here (e.g., play death animation, disable controls)
-            Debug.Log("Player entered death zone!");
 
-            // Reload the scene after a delay
-            Invoke("ReloadScene", delayBeforeReload);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
         }
-    }
-
-    private void ReloadScene()
-    {
-        // Reload the current scene
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
     
