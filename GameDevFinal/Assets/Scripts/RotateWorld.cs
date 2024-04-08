021f625508f6721a4995bb0946137a3b6df75a12
zkaplan1103 <zkaplan1103@gmail.com>
2024-04-08 16:52:18 +0200

    BIG CHANGES

--- GameDevFinal/Assets/Scripts/RotateWorld.cs
+++ GameDevFinal/Assets/Scripts/RotateWorld.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class RotateWorld : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void RotateWorldMethod(Vector3 degrees)
     {
-        
+        transform.Rotate(degrees);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
