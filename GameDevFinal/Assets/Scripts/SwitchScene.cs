0debba686cbe2882d6b4c2ce1b7d57496fe8284b
zkaplan1103 <zkaplan1103@gmail.com>
2024-04-25 23:08:31 +0200

    scene change

--- GameDevFinal/Assets/Scripts/SwitchScene.cs
+++ GameDevFinal/Assets/Scripts/SwitchScene.cs
@@ -3,23 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class NewBehaviourScript : MonoBehaviour
+
+public class SceneChangeOnCollision : MonoBehaviour
 {
+    // Define the name of the scene to load
+    public int sceneBuildIndex;
 
-    public class SceneChangeOnCollision : MonoBehaviour
+    // Function called when a collider enters the trigger
+    private void OnTriggerEnter (Collider other)
     {
-        // Define the name of the scene to load
-        public int sceneBuildIndex;
-
-        // Function called when a collider enters the trigger
-        private void OnTriggerEnter (Collider other)
+        // Check if the collider belongs to the player
+        if (other.tag == "Player")
         {
-            // Check if the collider belongs to the player
-            if (other.tag == "Player")
-            {
-                // Load the specified scene
-                SceneManager.LoadScene(sceneBuildIndex, LoadSceneMode.Single);
-            }
+            // Load the specified scene
+            UnlockNewLevel();
+            SceneManager.LoadScene(sceneBuildIndex, LoadSceneMode.Single);
         }
     }
+
+    private void UnlockNewLevel()
+    {
+        if (SceneManager.GetActiveScene().buildIndex >= PlayerPrefs.GetInt("ReachedIndex"))
+        {
+            PlayerPrefs.SetInt("ReachedIndex", SceneManager.GetActiveScene().buildIndex + 1);
+            PlayerPrefs.SetInt("UnlockedLevel", PlayerPrefs.GetInt("UnlockedLevel", 1) + 1);
+            PlayerPrefs.Save();
+        }
+     }
 }
