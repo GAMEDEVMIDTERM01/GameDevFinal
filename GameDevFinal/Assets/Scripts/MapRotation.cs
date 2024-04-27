320336fffd49655d43086b194926315ff3bb9239
zkaplan1103 <zkaplan1103@gmail.com>
2024-04-27 19:23:19 +0200

    attempted fixes

--- GameDevFinal/Assets/Scripts/MapRotation.cs
+++ GameDevFinal/Assets/Scripts/MapRotation.cs
@@ -8,42 +8,51 @@ public class MapRotation : MonoBehaviour
     public GameObject world;
     public GameObject player;
 
-    //public Quaternion[] rotationColliders;
+    //public GameObject[] rotationColliders;
 
-    //private Quaternion bestRotation;
+    //private Transform bestRotation;
     //private Quaternion colliderRotation;
 
     private void OnTriggerEnter(Collider collision)
     {
         if (collision.gameObject.CompareTag(player.tag))
         {
-            Debug.Log("theres a collision detected");
+            //Debug.Log("theres a collision detected");
             world.GetComponent<RotateWorld>().OnPlayerDetected(transform.localRotation);
-
             //colliderRotation = transform.localRotation;
         }
     }
 
-    //void GetClosestRotation(Quaternion[] colliders)
-    //{
-    //    float closestDistanceSqr = Mathf.Infinity;
-    //    Vector3 currentPosition = player.transform.position;
-    //    foreach (Transform potentialRotation in colliders)
-    //    {
-    //        Vector3 directionToTarget = potentialRotation.position - currentPosition;
-    //        float dSqrToTarget = directionToTarget.sqrMagnitude;
-    //        if (dSqrToTarget < closestDistanceSqr)
-    //        {
-    //            closestDistanceSqr = dSqrToTarget;
-    //            bestRotation = potentialRotation;
-    //        }
-    //    }
-    //}
+    /*
 
-    //private void FixedUpdate()
-    //{
-    //    GetClosestRotation(rotationColliders);
-    //}
+    void GetClosestRotation(GameObject[] colliders)
+    {
+        Vector3 currentPosition = player.transform.position;
+        foreach (GameObject potentialRotation in colliders)
+        {
+            Transform potentialRotationTransform = potentialRotation.transform;
+            Vector3 directionToTarget = potentialRotationTransform.position;
+            if (directionToTarget == currentPosition)
+            {
+                bestRotation = potentialRotationTransform;
+            }
+        }
 
+        if(bestRotation == transform)
+        {
+            Debug.Log("player hit position with collision detection");
+            world.GetComponent<RotateWorld>().OnPlayerDetected(colliderRotation);
+        }
+        else
+        {
+            Debug.Log("player hit position without collision detection");
+            world.GetComponent<RotateWorld>().OnPlayerDetected(bestRotation.localRotation);
+        }
+    }
 
+    private void FixedUpdate()
+    {
+        GetClosestRotation(rotationColliders);
+    }
+    */
 }
